e9e719a9c96219f36129f75ea7f05da479619990
David Betancur <dbetancur@psc.edu>
2024-05-21 13:30:20 -0400

    Updated directory schema to PyPI packaging.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="airflow_slurm_executor",
+    version="0.0.1",
+    author="Sunset666 (David Betancur)",
+    author_email="sunset666@sunset666.net",
+    description="Pluging for Airflow to connect to slurm clusters",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/sunset666/Airflow-Slurm-Executor",
+    packages=find_packages(),
+    classifiers=["Programming Language :: Python :: 3",
+                 "License :: OSI Approved :: Apache Software License",
+                 "Operating System :: OS Independent"],
+    python_requires=">=3.6",
+    install_requires=["apache-airflow>=2.5.3"],
+)
